b92523ed78e60a443616a8834109702afb902a77
Sunset <4542794+1B0Y@users.noreply.github.com>
2025-10-05 14:57:20 -0700

    Batch 2

---
+++ onx-evp-a-lightbars/shared/config.lua
@@ -0,0 +1,7 @@
+CONFIG = {
+    disablePoliceScannerAudio = false, -- controls if the police scanner audio should play when in an emergency vehicle
+    disableGhostSirens = true, -- disables distant "ghost" sirens
+    enableSirenCollision = true, -- disable if you're having collision issues with the siren
+    checkIntervalSpeed = 250, -- how often in ms to check if a lightbar should ne attached / detached (lower number more responsive worse performance)
+    debug = false, -- display debug prints
+}
---
+++ onx-evp-b-livery-mods/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+dependency '/assetpacks'
+
+version '1.1.0'
+lua54 'yes'
+
+server_scripts {
+  'version.lua',
+}
+dependency '/assetpacks'
